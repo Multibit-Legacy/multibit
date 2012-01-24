7b0cf78f3a18caec4d8cdb36df5e29d3292c4d7d
Jim Burton <jim618@fastmail.co.uk>
2012-01-24 20:53:04 +0000

    added more localisation tests

--- src/test/java/org/multibit/view/LocaliserTest.java
+++ src/test/java/org/multibit/view/LocaliserTest.java
@@ -1,11 +1,11 @@
 /**
- * Copyright 2011 Thilo Planz
+ * Copyright 2011 multibit.org
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the MIT license (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *    http://opensource.org/licenses/mit-license.php
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.multibit.view;
 
 import java.util.Locale;
@@ -40,33 +39,49 @@ public class LocaliserTest extends TestCase {
     private String SUBSTITUTE_ONE_EXPECTED_SPANISH = "primero = " + SUBSTITUTE_ONE_DATA;
     private String SUBSTITUTE_TWO_EXPECTED_SPANISH = "primero = " + SUBSTITUTE_ONE_DATA + ", segundo = " + SUBSTITUTE_TWO_DATA;
 
-    private String CAPITAL_CITY_EXPECTED_PORTUGUESE = "Lisboa";
-    private String SUBSTITUTE_ONE_EXPECTED_PORTUGUESE = "Primeiro = " + SUBSTITUTE_ONE_DATA;
-    private String SUBSTITUTE_TWO_EXPECTED_PORTUGUESE = "Primeiro = " + SUBSTITUTE_ONE_DATA + ", Segundo = " + SUBSTITUTE_TWO_DATA;
+    private String CAPITAL_CITY_EXPECTED_RUSSIAN = "\u041B\u043E\u043D\u0434\u043E\u043D";
+    private String SUBSTITUTE_ONE_EXPECTED_RUSSIAN = "\u043F\u0435\u0440\u0432\u044B\u0439 = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_RUSSIAN = "\u043F\u0435\u0440\u0432\u044B\u0439 = " + SUBSTITUTE_ONE_DATA + ", \u0432\u0442\u043E\u0440\u043E\u0439 = " + SUBSTITUTE_TWO_DATA;
 
-    private String CAPITAL_CITY_EXPECTED_GERMAN = "Berlin";
-    private String SUBSTITUTE_ONE_EXPECTED_GERMAN = "erster = " + SUBSTITUTE_ONE_DATA;
-    private String SUBSTITUTE_TWO_EXPECTED_GERMAN = "erster = " + SUBSTITUTE_ONE_DATA + ", zweiter = " + SUBSTITUTE_TWO_DATA;
-
-    private String CAPITAL_CITY_EXPECTED_FRENCH = "Paris";
-    private String SUBSTITUTE_ONE_EXPECTED_FRENCH = "premier = " + SUBSTITUTE_ONE_DATA;
-    private String SUBSTITUTE_TWO_EXPECTED_FRENCH = "premier = " + SUBSTITUTE_ONE_DATA + ", deuxi\u00e8me = " + SUBSTITUTE_TWO_DATA;
+    private String CAPITAL_CITY_EXPECTED_SWEDISH = "Stockholm";
+    private String SUBSTITUTE_ONE_EXPECTED_SWEDISH = "f\u00f6rsta = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_SWEDISH = "f\u00f6rsta = " + SUBSTITUTE_ONE_DATA + ", andra = " + SUBSTITUTE_TWO_DATA;
+ 
+    private String CAPITAL_CITY_EXPECTED_NORWEGIAN = "Oslo";
+    private String SUBSTITUTE_ONE_EXPECTED_NORWEGIAN = "f\u00f8rste = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_NORWEGIAN = "f\u00f8rste = " + SUBSTITUTE_ONE_DATA + ", andre = " + SUBSTITUTE_TWO_DATA;
 
     private String CAPITAL_CITY_EXPECTED_ITALIAN = "Roma";
     private String SUBSTITUTE_ONE_EXPECTED_ITALIAN = "primo = " + SUBSTITUTE_ONE_DATA;
     private String SUBSTITUTE_TWO_EXPECTED_ITALIAN = "primo = " + SUBSTITUTE_ONE_DATA + ", secondo = " + SUBSTITUTE_TWO_DATA;
 
-    private String CAPITAL_CITY_EXPECTED_NORWEGIAN = "Oslo";
-    private String SUBSTITUTE_ONE_EXPECTED_NORWEGIAN = "f\u00f8rste = " + SUBSTITUTE_ONE_DATA;
-    private String SUBSTITUTE_TWO_EXPECTED_NORWEGIAN = "f\u00f8rste = " + SUBSTITUTE_ONE_DATA + ", andre = " + SUBSTITUTE_TWO_DATA;
+    private String CAPITAL_CITY_EXPECTED_FRENCH = "Paris";
+    private String SUBSTITUTE_ONE_EXPECTED_FRENCH = "premier = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_FRENCH = "premier = " + SUBSTITUTE_ONE_DATA + ", deuxi\u00e8me = " + SUBSTITUTE_TWO_DATA;
+
+    private String CAPITAL_CITY_EXPECTED_GERMAN = "Berlin";
+    private String SUBSTITUTE_ONE_EXPECTED_GERMAN = "erster = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_GERMAN = "erster = " + SUBSTITUTE_ONE_DATA + ", zweiter = " + SUBSTITUTE_TWO_DATA;
+
+    private String CAPITAL_CITY_EXPECTED_PORTUGUESE = "Lisboa";
+    private String SUBSTITUTE_ONE_EXPECTED_PORTUGUESE = "Primeiro = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_PORTUGUESE = "Primeiro = " + SUBSTITUTE_ONE_DATA + ", Segundo = " + SUBSTITUTE_TWO_DATA;
+
+    private String CAPITAL_CITY_EXPECTED_DUTCH = "Amsterdam";
+    private String SUBSTITUTE_ONE_EXPECTED_DUTCH = "eerste = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_DUTCH = "eerste = " + SUBSTITUTE_ONE_DATA + ", tweede = " + SUBSTITUTE_TWO_DATA;
+
+    private String CAPITAL_CITY_EXPECTED_LATVIAN = "Riga";
+    private String SUBSTITUTE_ONE_EXPECTED_LATVIAN = "pirmais = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_LATVIAN = "pirmais = " + SUBSTITUTE_ONE_DATA + ", otrais = " + SUBSTITUTE_TWO_DATA;
+
+    private String CAPITAL_CITY_EXPECTED_TURKISH = "Ankara";
+    private String SUBSTITUTE_ONE_EXPECTED_TURKISH = "ilk = " + SUBSTITUTE_ONE_DATA;
+    private String SUBSTITUTE_TWO_EXPECTED_TURKISH = "ilk = " + SUBSTITUTE_ONE_DATA + ", ikinci = " + SUBSTITUTE_TWO_DATA;
+
+ 
+     
     
-    private String CAPITAL_CITY_EXPECTED_SWEDISH = "Stockholm";
-    private String SUBSTITUTE_ONE_EXPECTED_SWEDISH = "f\u00f6rsta = " + SUBSTITUTE_ONE_DATA;
-    private String SUBSTITUTE_TWO_EXPECTED_SWEDISH = "f\u00f6rsta = " + SUBSTITUTE_ONE_DATA + ", andra = " + SUBSTITUTE_TWO_DATA;
-    
-    private String CAPITAL_CITY_EXPECTED_RUSSIAN = "\u041B\u043E\u043D\u0434\u043E\u043D";
-    private String SUBSTITUTE_ONE_EXPECTED_RUSSIAN = "\u043F\u0435\u0440\u0432\u044B\u0439 = " + SUBSTITUTE_ONE_DATA;
-    private String SUBSTITUTE_TWO_EXPECTED_RUSSIAN = "\u043F\u0435\u0440\u0432\u044B\u0439 = " + SUBSTITUTE_ONE_DATA + ", \u0432\u0442\u043E\u0440\u043E\u0439 = " + SUBSTITUTE_TWO_DATA;
     
     @Test
     public void testLocaliseEnglish() {
@@ -92,58 +107,14 @@ public class LocaliserTest extends TestCase {
     }
     
     @Test
-    public void testLocalisePortuguese() {
-        Localiser localiser = new Localiser(new Locale("pt"));
+    public void testLocaliseRussian() {
+        Localiser localiser = new Localiser(new Locale("ru"));
 
         assertNotNull(localiser);
        
-//        assertEquals(CAPITAL_CITY_EXPECTED_PORTUGUESE, localiser.getString(CAPITAL_CITY_KEY));
-        assertEquals(SUBSTITUTE_ONE_EXPECTED_PORTUGUESE, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
-        assertEquals(SUBSTITUTE_TWO_EXPECTED_PORTUGUESE, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
-    }
-    
-    @Test
-    public void testLocaliseGerman() {
-        Localiser localiser = new Localiser(new Locale("de"));
-
-        assertNotNull(localiser);
-       
-        assertEquals(CAPITAL_CITY_EXPECTED_GERMAN, localiser.getString(CAPITAL_CITY_KEY));
-        assertEquals(SUBSTITUTE_ONE_EXPECTED_GERMAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
-        assertEquals(SUBSTITUTE_TWO_EXPECTED_GERMAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
-    }
-    
-    @Test
-    public void testLocaliseFrench() {
-        Localiser localiser = new Localiser(new Locale("fr"));
-
-        assertNotNull(localiser);
-       
-        assertEquals(CAPITAL_CITY_EXPECTED_FRENCH, localiser.getString(CAPITAL_CITY_KEY));
-//        assertEquals(SUBSTITUTE_ONE_EXPECTED_FRENCH, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
-//        assertEquals(SUBSTITUTE_TWO_EXPECTED_FRENCH, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
-    }
-    
-    @Test
-    public void testLocaliseItalian() {
-        Localiser localiser = new Localiser(new Locale("it"));
-
-        assertNotNull(localiser);
-       
-        assertEquals(CAPITAL_CITY_EXPECTED_ITALIAN, localiser.getString(CAPITAL_CITY_KEY));
-        assertEquals(SUBSTITUTE_ONE_EXPECTED_ITALIAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
-        assertEquals(SUBSTITUTE_TWO_EXPECTED_ITALIAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
-    }
-    
-    @Test
-    public void testLocaliseNorwegian() {
-        Localiser localiser = new Localiser(new Locale("no"));
-
-        assertNotNull(localiser);
-       
-        assertEquals(CAPITAL_CITY_EXPECTED_NORWEGIAN, localiser.getString(CAPITAL_CITY_KEY));
-        assertEquals(SUBSTITUTE_ONE_EXPECTED_NORWEGIAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
-        assertEquals(SUBSTITUTE_TWO_EXPECTED_NORWEGIAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+        assertEquals(CAPITAL_CITY_EXPECTED_RUSSIAN, localiser.getString(CAPITAL_CITY_KEY));
+        assertEquals(SUBSTITUTE_ONE_EXPECTED_RUSSIAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+        assertEquals(SUBSTITUTE_TWO_EXPECTED_RUSSIAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
     }
     
     @Test
@@ -158,15 +129,93 @@ public class LocaliserTest extends TestCase {
     }
     
     @Test
-    public void testLocaliseRussian() {
-        Localiser localiser = new Localiser(new Locale("ru"));
+    public void testLocaliseNorwegian() {
+        Localiser localiser = new Localiser(new Locale("no"));
 
         assertNotNull(localiser);
        
-        assertEquals(CAPITAL_CITY_EXPECTED_RUSSIAN, localiser.getString(CAPITAL_CITY_KEY));
-        assertEquals(SUBSTITUTE_ONE_EXPECTED_RUSSIAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
-        assertEquals(SUBSTITUTE_TWO_EXPECTED_RUSSIAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+        assertEquals(CAPITAL_CITY_EXPECTED_NORWEGIAN, localiser.getString(CAPITAL_CITY_KEY));
+        assertEquals(SUBSTITUTE_ONE_EXPECTED_NORWEGIAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+        assertEquals(SUBSTITUTE_TWO_EXPECTED_NORWEGIAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
     }
+    
+    @Test
+    public void testLocaliseItalian() {
+        Localiser localiser = new Localiser(new Locale("it"));
+
+        assertNotNull(localiser);
+       
+        assertEquals(CAPITAL_CITY_EXPECTED_ITALIAN, localiser.getString(CAPITAL_CITY_KEY));
+        assertEquals(SUBSTITUTE_ONE_EXPECTED_ITALIAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+        assertEquals(SUBSTITUTE_TWO_EXPECTED_ITALIAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+    }
+    
+    @Test
+    public void testLocaliseFrench() {
+        Localiser localiser = new Localiser(new Locale("fr"));
+
+        assertNotNull(localiser);
+       
+        assertEquals(CAPITAL_CITY_EXPECTED_FRENCH, localiser.getString(CAPITAL_CITY_KEY));
+        //assertEquals(SUBSTITUTE_ONE_EXPECTED_FRENCH, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+        //assertEquals(SUBSTITUTE_TWO_EXPECTED_FRENCH, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+    }
+ 
+   @Test
+    public void testLocaliseGerman() {
+        Localiser localiser = new Localiser(new Locale("de"));
+
+        assertNotNull(localiser);
+       
+        assertEquals(CAPITAL_CITY_EXPECTED_GERMAN, localiser.getString(CAPITAL_CITY_KEY));
+        assertEquals(SUBSTITUTE_ONE_EXPECTED_GERMAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+        assertEquals(SUBSTITUTE_TWO_EXPECTED_GERMAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+    }
+    
+   @Test
+   public void testLocalisePortuguese() {
+       Localiser localiser = new Localiser(new Locale("pt"));
+
+       assertNotNull(localiser);
+      
+       assertEquals(CAPITAL_CITY_EXPECTED_PORTUGUESE, localiser.getString(CAPITAL_CITY_KEY));
+       assertEquals(SUBSTITUTE_ONE_EXPECTED_PORTUGUESE, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+       assertEquals(SUBSTITUTE_TWO_EXPECTED_PORTUGUESE, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+   }
+    
+   @Test
+   public void testLocaliseDutch() {
+       Localiser localiser = new Localiser(new Locale("nl"));
+
+       assertNotNull(localiser);
+      
+       //assertEquals(CAPITAL_CITY_EXPECTED_DUTCH, localiser.getString(CAPITAL_CITY_KEY));
+       assertEquals(SUBSTITUTE_ONE_EXPECTED_DUTCH, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+       assertEquals(SUBSTITUTE_TWO_EXPECTED_DUTCH, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+   }
+   
+   @Test
+   public void testLocaliseLatvian() {
+       Localiser localiser = new Localiser(new Locale("lv"));
+
+       assertNotNull(localiser);
+      
+       assertEquals(CAPITAL_CITY_EXPECTED_LATVIAN, localiser.getString(CAPITAL_CITY_KEY));
+       assertEquals(SUBSTITUTE_ONE_EXPECTED_LATVIAN, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+       assertEquals(SUBSTITUTE_TWO_EXPECTED_LATVIAN, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+   }
+    
+   
+   @Test
+   public void testLocaliseTurkish() {
+       Localiser localiser = new Localiser(new Locale("tr"));
+
+       assertNotNull(localiser);
+      
+       //assertEquals(CAPITAL_CITY_EXPECTED_TURKISH, localiser.getString(CAPITAL_CITY_KEY));
+       assertEquals(SUBSTITUTE_ONE_EXPECTED_TURKISH, localiser.getString(SUBSTITUTE_ONE_KEY, new Object[]{SUBSTITUTE_ONE_DATA}));
+       assertEquals(SUBSTITUTE_TWO_EXPECTED_TURKISH, localiser.getString(SUBSTITUTE_TWO_KEY, new Object[]{SUBSTITUTE_ONE_DATA, SUBSTITUTE_TWO_DATA}));                    
+   }
     
     @Test
     public void testVersionNumber() {
