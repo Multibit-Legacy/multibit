5283b68edc05e9723160af970488476d81f0b6e5
Jim Burton <jim618@fastmail.co.uk>
2012-08-16 10:34:00 +0100

    early work

---
+++ src/test/java/org/multibit/viewsystem/swing/action/DeleteSendingAddressActionTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2012 multibit.org
+ *
+ * Licensed under the MIT license (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/mit-license.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.multibit.viewsystem.swing.action;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.multibit.controller.MultiBitController;
+import org.multibit.message.Message;
+import org.multibit.message.MessageManager;
+import org.multibit.viewsystem.swing.view.SendBitcoinPanel;
+import org.multibit.viewsystem.swing.view.components.FontSizer;
+
+public class DeleteSendingAddressActionTest extends TestCase {
+    @Test
+    public void testDeleteSendingAddress() throws Exception {
+        // Create MultiBit controller.
+        MultiBitController controller = ActionTestUtils.createController();
+
+        // Create a new active wallet.
+        assertTrue("There was an active wallet when there should not be", controller.getModel().thereIsNoActiveWallet());
+
+        // Create a new CreateNewSendingAddressAction to test.
+        FontSizer.INSTANCE.initialise(controller);
+        SendBitcoinPanel sendBitcoinPanel = new SendBitcoinPanel(null, controller);
+        CreateNewSendingAddressAction createNewSendingAddressAction = sendBitcoinPanel.getCreateNewSendingAddressAction();
+
+        assertNotNull("createNewSendingAddressAction was not created successfully", createNewSendingAddressAction);
+
+        // Execute.
+        createNewSendingAddressAction.actionPerformed(null);
+        Object[] messages = MessageManager.INSTANCE.getMessages().toArray();
+        assertTrue("There were no messages but there should have been", messages != null && messages.length > 0);
+        assertEquals("Wrong message after send bitcoin confirm with no active wallet", ResetTransactionsSubmitActionTest.EXPECTED_NO_WALLET_IS_SELECTED, ((Message)messages[messages.length - 1]).getText());
+    }
+}
