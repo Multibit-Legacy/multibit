51b9a8d6e9022bab55c0b2c54f08929ff8ac0bfa
Cameron Garnham <da2ce7@gmail.com>
2013-01-10 17:43:52 +1100

    make IView for more views

--- src/main/java/org/multibit/viewsystem/View.java
+++ src/main/java/org/multibit/viewsystem/View.java
@@ -25,61 +25,26 @@ import javax.swing.Icon;
  * @author jim
  *
  */
-public interface View {
-    public static final int SAME_VIEW = -1;                 // Not a real view - used to forward to the same view as calling
-    public static final int UNKNOWN_VIEW = 0;
-    public static final int TRANSACTIONS_VIEW = 1;
-    public static final int SEND_BITCOIN_VIEW = 2;
-    public static final int SEND_BITCOIN_CONFIRM_VIEW = 3;  // obsolete - now done with Swing dialog
-    public static final int RECEIVE_BITCOIN_VIEW = 4;
-    public static final int HELP_CONTENTS_VIEW = 5;
-    public static final int HELP_ABOUT_VIEW = 6;
-    public static final int PREFERENCES_VIEW = 7; 
-    public static final int OPEN_WALLET_VIEW = 8;           // obsolete - now done with Swing dialog
-    public static final int SAVE_WALLET_AS_VIEW = 9;        // obsolete - now done with Swing dialog
-    public static final int VALIDATION_ERROR_VIEW = 10;     // obsolete - now done with Swing dialog
-    public static final int YOUR_WALLETS_VIEW = 11;
-    public static final int CREATE_BULK_ADDRESSES_VIEW = 12;  // obsolete
-    public static final int RESET_TRANSACTIONS_VIEW = 13;
-    public static final int SHOW_OPEN_URI_DIALOG_VIEW = 14;
-    public static final int SHOW_IMPORT_PRIVATE_KEYS_VIEW = 15;
-    public static final int SHOW_EXPORT_PRIVATE_KEYS_VIEW = 16;
-    public static final int WELCOME_VIEW = 17;
-    public static final int MESSAGES_VIEW = 18;
-    public static final int ADD_PASSWORD_VIEW = 19;
-    public static final int CHANGE_PASSWORD_VIEW = 20;
-    public static final int REMOVE_PASSWORD_VIEW = 21;
-    public static final int CHARTS_VIEW = 22;
-    
-    public static final int DEFAULT_VIEW = WELCOME_VIEW;
-     
-    /**
-     * display the view
-     */
-    public void displayView();
-    
-    /**
-     * Navigate away from the view (including releasing any resources used)
-     */
-    public void navigateAwayFromView();  
-    
-    /**
-     * @returns the icon for the view
-     */
-    public Icon getViewIcon();
-    
-    /**
-     * @returns the title for the view
-     */
-    public String getViewTitle();
-    
-    /**
-     * @returns the tooltip for the view
-     */
-    public String getViewTooltip();
-    
-    /**
-     * @returns the view identifier for the view
-     */
-    public int getViewId();
+public interface View extends IView{
+    public static final int TRANSACTIONS_VIEW = 101;
+    public static final int SEND_BITCOIN_VIEW = 102;
+    public static final int SEND_BITCOIN_CONFIRM_VIEW = 103;  // obsolete - now done with Swing dialog
+    public static final int RECEIVE_BITCOIN_VIEW = 104;
+    public static final int HELP_CONTENTS_VIEW = 105;
+    public static final int HELP_ABOUT_VIEW = 106;
+    public static final int PREFERENCES_VIEW = 107; 
+    public static final int OPEN_WALLET_VIEW = 108;           // obsolete - now done with Swing dialog
+    public static final int SAVE_WALLET_AS_VIEW = 109;        // obsolete - now done with Swing dialog
+    public static final int VALIDATION_ERROR_VIEW = 110;     // obsolete - now done with Swing dialog
+    public static final int YOUR_WALLETS_VIEW = 111;
+    public static final int CREATE_BULK_ADDRESSES_VIEW = 112;  // obsolete
+    public static final int RESET_TRANSACTIONS_VIEW = 113;
+    public static final int SHOW_OPEN_URI_DIALOG_VIEW = 114;
+    public static final int SHOW_IMPORT_PRIVATE_KEYS_VIEW = 115;
+    public static final int SHOW_EXPORT_PRIVATE_KEYS_VIEW = 116;
+    public static final int MESSAGES_VIEW = 118;
+    public static final int ADD_PASSWORD_VIEW = 119;
+    public static final int CHANGE_PASSWORD_VIEW = 120;
+    public static final int REMOVE_PASSWORD_VIEW = 121;
+    public static final int CHARTS_VIEW = 122;
 }
