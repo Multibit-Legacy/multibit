d3a70e26e8cb22bbac0601ac4a0d21edabed791b
Jim Burton <jim618@fastmail.co.uk>
2013-01-29 18:48:18 +0000

    removed onTransaction wallet looping - caused deadlock and no longer necessary.

--- src/main/java/org/multibit/controller/MultiBitPeerEventListener.java
+++ src/main/java/org/multibit/controller/MultiBitPeerEventListener.java
@@ -2,7 +2,6 @@ package org.multibit.controller;
 
 import java.util.List;
 
-import org.multibit.model.PerWalletModelData;
 import org.multibit.model.StatusEnum;
 import org.multibit.viewsystem.swing.view.panels.SendBitcoinConfirmPanel;
 import org.slf4j.Logger;
@@ -13,10 +12,7 @@ import com.google.bitcoin.core.GetDataMessage;
 import com.google.bitcoin.core.Message;
 import com.google.bitcoin.core.Peer;
 import com.google.bitcoin.core.PeerEventListener;
-import com.google.bitcoin.core.ScriptException;
 import com.google.bitcoin.core.Transaction;
-import com.google.bitcoin.core.VerificationException;
-import com.google.bitcoin.core.Wallet;
 
 public class MultiBitPeerEventListener implements PeerEventListener {
 
@@ -68,41 +64,6 @@ public class MultiBitPeerEventListener implements PeerEventListener {
 
     @Override
     public void onTransaction(Peer peer, Transaction transaction) { 
-        // Loop through all the wallets, seeing if the transaction is relevant
-        // and adding them as pending if so.
-        // (As of 25 Oct 2012, intrawallet zero confirmation tx are not seen if this code is removed)
-        if (transaction != null) {
-            try {
-                java.util.List<PerWalletModelData> perWalletModelDataList = controller.getModel().getPerWalletModelDataList();
-
-                if (perWalletModelDataList != null) {
-                    for (PerWalletModelData perWalletModelData : perWalletModelDataList) {
-                        Wallet loopWallet = perWalletModelData.getWallet();
-                        if (loopWallet != null) {
-                            if (loopWallet.isTransactionRelevant(transaction)) {
-                                // The perWalletModelData is marked as dirty.
-                                if (perWalletModelData.getWalletInfo() != null) {
-                                    synchronized(perWalletModelData.getWalletInfo()) {
-                                        perWalletModelData.setDirty(true);
-                                    }
-                                } else {
-                                    perWalletModelData.setDirty(true);
-                                }
-                                if (loopWallet.getTransaction(transaction.getHash()) == null) {
-                                    log.debug("MultiBit adding a new pending transaction for the wallet '"
-                                            + perWalletModelData.getWalletDescription() + "'\n" + transaction.toString());
-                                    loopWallet.receivePending(transaction);
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch (ScriptException e) {
-                log.error(e.getMessage(), e);
-            } catch (VerificationException e) {
-                log.error(e.getMessage(), e);
-            }
-        }
     }
 
     @Override
