3bc64c8743d980c7bd97157549864fdcdecb3f99
Cameron Garnham <da2ce7@gmail.com>
2013-02-13 23:46:41 +1100

    add BaseModel

---
+++ src/main/java/org/multibit/model/BaseModel.java
@@ -0,0 +1,84 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 Cameron Garnham.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.multibit.model;
+
+import java.util.Properties;
+
+/**
+ *
+ * @author Cameron Garnham
+ */
+public abstract class BaseModel<M extends BaseModel<M>> implements Model {
+    
+    protected Properties userPreferences;
+    
+    protected BaseModel(Properties userPreferences) {
+        
+        if (null == userPreferences) {
+            throw new NullPointerException();
+        }
+        
+        this.userPreferences = userPreferences;
+    }
+    
+    /**
+     * Get a user preference.
+     *
+     * @param key String key of property
+     * @return String property value
+     */
+    @Override
+    public final String getUserPreference(String key) {
+        return userPreferences.getProperty(key);
+    }
+
+    /**
+     * Set a user preference.
+     *
+     * @return
+     */
+    @Override
+    public void setUserPreference(String key, String value) {
+        if (key != null && value != null) {
+            userPreferences.put(key, value);
+        }
+    }
+
+    /**
+     * Get all user preference.
+     *
+     * @return
+     */
+    @Override
+    public Properties getAllUserPreferences() {
+        return userPreferences;
+    }
+
+    /**
+     * Set all user preferences.
+     */
+    public void setAllUserPreferences(Properties properties) {
+        userPreferences = properties;
+    }
+}
