03e049935642aae8d93f940be17e239c57157d37
Cameron Garnham <da2ce7@gmail.com>
2013-02-14 00:05:29 +1100

    refactor view into BaseModel

--- src/main/java/org/multibit/model/BaseModel.java
+++ src/main/java/org/multibit/model/BaseModel.java
@@ -24,24 +24,91 @@
 package org.multibit.model;
 
 import java.util.Properties;
+import org.multibit.viewsystem.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Cameron Garnham
  */
 public abstract class BaseModel<M extends BaseModel<M>> implements Model {
+
+    private static final Logger log = LoggerFactory.getLogger(BaseModel.class);
     
+    // User preferences.
+    public static final String SELECTED_VIEW = "selectedView";
+    public static final String SELECTED_VIEW_ENUM = "selectedViewEnum";
+    public static final String PREVIOUSLY_SELECTED_VIEW = "previousView";
+    
+    
+    /**
+     * Where most of the local-user settings are stored.
+     */
     protected Properties userPreferences;
     
+    /**
+     * The currently displayed view. One of the View constants.
+     */
+    private View currentView = null;
+
     protected BaseModel(Properties userPreferences) {
-        
+
         if (null == userPreferences) {
             throw new NullPointerException();
         }
-        
+
         this.userPreferences = userPreferences;
+
+        this.SetSavedView();
     }
-    
+
+    private void SetSavedView() {
+        // Initialize everything to look at the stored opened view.
+        // If no properties passed in just initialize to the default view.
+        if (userPreferences != null) {
+            // first try and find a old view setting.
+            View initialViewInProperties = null;
+            Object oldViewObject = userPreferences.get(MultiBitModel.SELECTED_VIEW);
+
+            String oldViewString = (null != oldViewObject) ? (String) oldViewObject : null;
+
+            if (null != oldViewString) {
+                Integer oldViewInt = null;
+                try {
+                    oldViewInt = Integer.parseInt(oldViewString);
+                } catch (NumberFormatException nfe) {
+                    // do nothing
+                } finally {
+                    initialViewInProperties = View.parseOldView(oldViewInt);
+                    
+                    // Remove the old view property from the properties - replaced by enum.
+                    // (It may be put back in for backwads compatibility in FileHandler#writeUserPreferences.
+                    userPreferences.remove(MultiBitModel.SELECTED_VIEW);
+                }
+            }
+
+            // If oldViewInProperties is still null,  try and find the view.
+            if (null == initialViewInProperties) {
+                Object viewObject = userPreferences.get(MultiBitModel.SELECTED_VIEW_ENUM);
+
+                String viewString = (null != viewObject) ? (String) viewObject : null;
+
+                if (viewString != null) {
+                    try {
+                        View viewEnum = View.valueOf(viewString);
+                        initialViewInProperties = (!viewEnum.isObsolete()) ? viewEnum : null;
+
+                    } catch (IllegalArgumentException nfe) {
+                        // do nothing.
+                    }
+                }
+            }
+            setCurrentView((null != initialViewInProperties) ? initialViewInProperties : View.DEFAULT_VIEW());
+            log.debug("Initial view from properties file is '" + getCurrentView().toString() + "'");
+        }
+    }
+
     /**
      * Get a user preference.
      *
@@ -80,5 +147,16 @@ public abstract class BaseModel<M extends BaseModel<M>> implements Model {
      */
     public void setAllUserPreferences(Properties properties) {
         userPreferences = properties;
+    }
+
+    @Override
+    public final View getCurrentView() {
+        return currentView;
+    }
+
+    @Override
+    public final void setCurrentView(View view) {
+        this.currentView = view;
+        this.setUserPreference(SELECTED_VIEW_ENUM, view.name());
     }
 }
