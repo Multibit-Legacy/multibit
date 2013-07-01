ac757f9d606779a97165272287278183d1aa7e19
Jim Burton <jim618@fastmail.co.uk>
2013-07-01 19:02:54 +0100

    now logs version of MultiBit

--- src/main/java/org/multibit/MultiBit.java
+++ src/main/java/org/multibit/MultiBit.java
@@ -207,6 +207,9 @@ public class MultiBit {
                 }
             }
             coreController.setLocaliser(localiser);
+            
+            log.debug("MultiBit version = " + localiser.getVersionNumber());
+
 
             log.debug("Creating model");
 
@@ -258,35 +261,6 @@ public class MultiBit {
             ColorAndFontConstants.init();
             FontSizer.INSTANCE.initialise(controller);
             CurrencyConverter.INSTANCE.initialise(finalController);
-            
-            // Check the fee is between the lower and upper bounds - set to default fee if not.
-//            String sendFeeString = controller.getModel().getUserPreference(BitcoinModel.SEND_FEE);
-//            boolean setToDefaultFee = false;
-//
-//            if (sendFeeString == null || sendFeeString == "") {
-//                setToDefaultFee = true;
-//            } else {
-//                CurrencyConverterResult converterResult = CurrencyConverter.INSTANCE.parseToBTCNotLocalised(sendFeeString);
-//
-//                if (converterResult.isBtcMoneyValid()) {
-//                    // Check that the fee is at least the minimum fee and
-//                    // smaller than the maximum fee.
-//                    BigInteger feeAsBigInteger = converterResult.getBtcMoney().getAmount().toBigInteger();
-//                    if (feeAsBigInteger.compareTo(BitcoinModel.SEND_MINIMUM_FEE) < 0) {
-//                        setToDefaultFee = true;
-//                    } else {
-//                        if (feeAsBigInteger.compareTo(BitcoinModel.SEND_MAXIMUM_FEE) >= 0) {
-//                            setToDefaultFee = true;
-//                        }
-//                    }
-//                }
-//            }
-//
-//            if (setToDefaultFee) {
-//               sendFeeString = controller.getLocaliser()
-//                        .bitcoinValueToStringNotLocalised(BitcoinModel.SEND_FEE_DEFAULT, false, false);
-//               controller.getModel().setUserPreference(BitcoinModel.SEND_FEE, sendFeeString);
-//            }
 
             // This is when the GUI is first displayed to the user.
             log.debug("Creating user interface with initial view : " + controller.getCurrentView());
