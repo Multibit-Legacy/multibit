7366a4de2b8a91f2ab369ec2e3992baf69f4c3f9
Gary Rowe <g.rowe@froot.co.uk>
2013-08-01 14:22:02 +0100

    Removed unnecessary case change (just use ignore case)

--- src/main/java/org/multibit/viewsystem/swing/view/components/JFontChooser.java
+++ src/main/java/org/multibit/viewsystem/swing/view/components/JFontChooser.java
@@ -288,12 +288,11 @@ public class JFontChooser extends JComponent {
      * @param name
      *            the family name of the selected font.
      * 
-     * @see getSelectedFontFamily
      **/
     public void setSelectedFontFamily(String name) {
         String[] names = getFontFamilies();
         for (int i = 0; i < names.length; i++) {
-            if (names[i].toLowerCase().equals(name.toLowerCase())) {
+            if (names[i].equalsIgnoreCase(name.toLowerCase())) {
                 getFontFamilyList().setSelectedIndex(i);
                 break;
             }
