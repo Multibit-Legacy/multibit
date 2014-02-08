8190990f8e946f741581200e64b3a6384db09112
langerhans <max.keller@gmx.com>
2014-02-08 20:04:30 +0100

    Update Help links

--- src/main/java/org/multibit/viewsystem/swing/view/panels/HelpContentsPanel.java
+++ src/main/java/org/multibit/viewsystem/swing/view/panels/HelpContentsPanel.java
@@ -41,7 +41,7 @@ public class HelpContentsPanel extends JPanel implements Viewable {
     //public static final String HELP_BASE_URL = "http://www.multibit.org/";
     
     // Version 0.5.x - live
-    public static final String HELP_BASE_URL = "http://www.multidoge.org/v0.5/";
+    public static final String HELP_BASE_URL = "http://multidoge.org/help/v0.1/";
     
     
     // Version 0.4.x - test
